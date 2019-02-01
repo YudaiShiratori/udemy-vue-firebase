5d8ad0f4096f8544fb6089aad60d68469877080d
shiratori yudai <toritorikebabu1023x@gmail.com>
2019-02-01 13:09:12 +0900

    The Vue CLI  until23

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <Navbar />
+    <AllFriends :friends='friends' @delete ='deleteFriend'/>
+    <OnlineFriends :friends='friends' />
+    <Blogs />
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+import AllFriends from './AllFriends'
+import OnlineFriends from './OnlineFriends'
+import Blogs from './Blogs'
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    AllFriends,
+    OnlineFriends,
+    Blogs
+  },
+  data () {
+    return {
+      title: 'Vue+Firebase',
+      friends: [
+        { name: 'aa', online: true },
+        { name: 'bb', online: false },
+        { name: 'cc', online: true },
+        { name: 'dd', online: false }
+      ],
+      methods: {
+        deletefriend (payload) {
+          console.log(payload.friend)
+          this.friends = this.friends.filter(friend => {
+            return friend.name !== payload.name
+          })
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+/* eslint-disable */
---
+++ src/Blogs.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="blog">
+    <h2>{{ BlogTitle }}</h2>
+    <button @click='changeTitle'>Change Title</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Blogs',
+  data() {
+    return {
+      BlogTitle: 'Como estas?'
+    }
+  },
+  methods: {
+    changeTitle() {
+      this.BlogTitle = 'Bien!'
+    }
+  },
+  beforeCreate() {
+    alert('beforeCreate')
+  },
+  created() {
+    alert('created')
+  },
+  beforeUpdate() {
+    alert('beforeUpdate')
+  }
+}
+</script>
